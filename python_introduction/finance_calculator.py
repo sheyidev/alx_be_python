902d84d2b3a1aa045541931839c3ad2cebe1166c
John <j.gaji@meneses.law>
2024-06-21 14:44:58 -0500

    edited

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,7 +7,7 @@ It will then project these savings over a year, assuming a fixed interest rate, 
 income = int(input("Enter your monthly income: "))
 expenses = int(input("Enter your total monthly expenses: "))
 
-savings = income - expenses
-projected_savings = (savings * 12) + (savings * 12 * 0.05)
-print(f"Your monthly savings is ${savings}.")
+monthly_savings = income - expenses
+projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings:.0f}.")
