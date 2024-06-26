f1b09987dec0bb9d24469a4149a127595eda0e95
John <j.gaji@meneses.law>
2024-06-26 14:11:15 -0500

    add int to input

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1,11):
     product = number * i
-    print(f"{number} * {i} = {product}")
+   # print(f"{number} * {i} = {product}")
+    print(str(number) + "*" + str(i) + "=" + str(product))
