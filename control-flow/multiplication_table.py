fc063d669a3f02d2e3123d70ac4d00625ba97c52
Sheyi <seezie@gmail.com>
2024-06-27 21:25:07 -0500

    concatenate

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,5 @@ number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1,11):
     product = number * i
    
-    print(str(number) + " " + "*" + " " + str(i) + " "+ "=" + " " + str(product))
+   # print(str(number) + " " + "*" + " " + str(i) + " "+ "=" + " " + str(product))
+    print(f"{number} * {i} = {product}")
