08b4982763eb1aa78e55edcdc9b212d215ed8e8a
Sheyi <seezie@gmail.com>
2024-06-27 21:44:22 -0500

    asterisks

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,9 @@
 ## Create a Python script named multiplication_table.py. This script will ask the user to enter a number, then use a for loop to print the multiplication table for that number from 1 to 10.
-number = input("Enter a number to see its multiplication table: ")
+number = int(input("Enter the size of the pattern: "))
 
-for i in range(1, 11):
-    result = i * number
-    print(result)
+row = 0
+while row < number:
+    for col in range(number):
+        print(f"*", end="")
+    print()
+    row += 1
