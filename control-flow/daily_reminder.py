1ffb66e2ff75c128c29b11c15df49ec3a710c615
John <j.gaji@meneses.law>
2024-07-01 14:33:29 -0500

    edited a file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,10 +5,10 @@
 ## demonstrating control flow and loops without relying on data structures to store multiple tasks.
 
 user_task = str(input("Enter your task: "))
-user_priority = str(input("Priority (high/medium/low: )"))
+priority = str(input("Priority (high/medium/low: )"))
 user_timebound = str(input("Is it time-bound? (yes/no):"))
 
-match user_priority:
+match priority :
     case "high":
         if user_timebound == "yes":
           print(f"Reminder: {user_task} is a high priority task that requires immediate attention today!")
