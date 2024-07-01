c4700ca7fbabc603b9fbe20419d303e5d63c5707
John <j.gaji@meneses.law>
2024-07-01 14:21:04 -0500

    personal daily reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,19 @@
+## Develop a script named daily_reminder.py. 
+## This script will ask the user for a single task, its priority level, 
+## and if it is time-sensitive. 
+## The program will then provide a customized reminder for that task,
+## demonstrating control flow and loops without relying on data structures to store multiple tasks.
+
+user_task = str(input("Enter your task: "))
+user_priority = str(input("Proriority (high/medium/low: )"))
+user_timebound = str(input("Is it time-bound (yes/no): "))
+
+match user_priority:
+    case "high":
+        if user_timebound == "yes":
+          print(f"Reminder: {user_task} is a high priority task that requires immediate attention today!")
+    case "low":
+       if user_timebound =="no":
+          print(f"{user_task} is a low priority task. Consider complemting it when you free time.")
+    case _:
+      print("print,no task given.")
--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,4 +1,6 @@
-## Create a Python script named multiplication_table.py. This script will ask the user to enter a number, then use a for loop to print the multiplication table for that number from 1 to 10.
+## Create a Python script named multiplication_table.py.
+## This script will ask the user to enter a number, 
+## then use a for loop to print the multiplication table for that number from 1 to 10.
 number = int(input("Enter the size of the pattern: "))
 
 row = 0
