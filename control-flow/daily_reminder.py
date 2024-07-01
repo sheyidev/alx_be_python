89875ebe822cd872f6e934a3c9cb5eec19371ef9
John <j.gaji@meneses.law>
2024-07-01 14:39:25 -0500

    edited a file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,16 +4,16 @@
 ## The program will then provide a customized reminder for that task,
 ## demonstrating control flow and loops without relying on data structures to store multiple tasks.
 
-user_task = str(input("Enter your task: "))
+task = str(input("Enter your task: "))
 priority = str(input("Priority (high/medium/low: )"))
-user_timebound = str(input("Is it time-bound? (yes/no):"))
+time_bound = str(input("Is it time-bound? (yes/no):"))
 
 match priority :
     case "high":
-        if user_timebound == "yes":
-          print(f"Reminder: {user_task} is a high priority task that requires immediate attention today!")
+        if time_bound == "yes":
+          print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
     case "low":
-       if user_timebound =="no":
-          print(f"{user_task} is a low priority task. Consider completing it when you free time.")
+       if time_bound =="no":
+          print(f"{task} is a low priority task. Consider completing it when you free time.")
     case _:
       print("print,no task given.")
