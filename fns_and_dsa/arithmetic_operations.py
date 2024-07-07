3f503c4bd78c148fb2ad800f0caecfc48703b87f
Sheyi <seezie@gmail.com>
2024-07-07 16:18:48 -0500

    arithmetic ops

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,25 @@
 def perform_operation(num1, num2):
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    add = num1 + num2
-    subtract = num1 - num2
-    multiply = num1 * num2
-    divide = 
+    operation = input("choose the operation (+, -, *, /): ")
 
-   
+
+    match operation:
+        case '+':
+            result = num1 + num2
+            print(f"The result is {result}")
+        case "-":
+            result = num1 - num2
+            print(f"The result is {result}")
+        case "*":
+            result = num1 * num2
+            print(f"The result is {result}")
+        case "/":
+            result = num1 / num2
+            if num2 == 0:
+                print("Cannot divide by zero")
+            else:
+                print(f"The result is {result}")
+        case _:
+            print("Invalid day entered.")
+    
