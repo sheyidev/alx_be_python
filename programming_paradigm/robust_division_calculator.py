d2178b9c683d45f3d1526cd90e81fe35aae3b1e3
John <j.gaji@meneses.law>
2024-07-18 15:30:42 -0500

    working on paradigm

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,17 +1,18 @@
 ## Objective: Implement a division calculator that
 ## robustly handles errors like division by zero and non-numeric inputs using command line arguments.
 def safe_divide(numerator, denominator):
-    numerator = float(numerator)
-    denominator = float(denominator)
+  
     try:
+        numerator =   float(numerator)
+        denominator = float(denominator)
         if denominator == 0:
             raise ZeroDivisionError(f"Error: Cannot divide by zero.")
         
         else:
-            result = numerator /denominator
+            result = numerator / denominator
             return f"The result of the division is {result:.1f}"
-    except ValueError:
-        print("Error: Please enter numeric values only.")
+    except ValueError as e:
+        return f"Error: Please enter numeric values only."
     except ZeroDivisionError as e:
         return str(e)
 
