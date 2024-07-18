e34822fc957ec43a264e1443a59c2c8249884e5e
John <j.gaji@meneses.law>
2024-07-18 16:25:41 -0500

    working on paradigm

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,8 +7,19 @@ from simple_calculator import SimpleCalculator
 class TestAdd(unittest.TestCase):
     def test_addition(self):
         self.calc = SimpleCalculator()
-        result = self.calc.add(2, 4)
+        #result = self.calc.add(2, 4)
         self.assertEqual(self.calc.add(2, 4), 6)
+    def test_subtraction(self):
+        self.calc = SimpleCalculator()
+    
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+    def test_multiplication(self):
+        self.calc = SimpleCalculator()
 
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+    def test_division(self):
+        self.calc = SimpleCalculator()
+
+        self.assertEqual(self.calc.divide(10, 5), 2)
 if __name__ == "__main__":
     unittest.main()
