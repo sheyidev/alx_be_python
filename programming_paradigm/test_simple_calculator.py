f7af1e3fde2ab3fdacf11efd95c87e6b32f5774d
John <j.gaji@meneses.law>
2024-07-18 16:02:54 -0500

    working on paradigm

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,13 @@
+## Objective: Learn the basics of unit testing in Python by writing tests for a provided SimpleCalculator
+## class that supports addition, subtraction, multiplication, and division operations.
+# simple_calculator.py
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        result = SimpleCalculator()
+        result = result.add(2, 4)
+        self.assertEqual(result, 6)
+
+
