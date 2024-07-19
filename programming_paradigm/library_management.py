f9346fa5842e0c0e1b3534b822dc64f98a79fdc9
John <j.gaji@meneses.law>
2024-07-19 13:56:36 -0500

    renamed

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,9 +1,3 @@
-## Objective: Solidify understanding of basic OOP concepts in Python by implementing a system that tracks books in a library, focusing on classes, 
-## object instantiation, and method invocation.
-
-##Implement a Book class with public attributes title and author, 
-# and a private attribute _is_checked_out to track its availability.
-
 class Book:
     def __init__(self, title, author):
         self.title = title
